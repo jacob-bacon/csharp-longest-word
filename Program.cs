0c50c07f6854eda9eb077a58b5867bf7fc66817a
Jacob Bacon <36112428+jacob-bacon@users.noreply.github.com>
2018-02-10 14:00:34 -0600

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace FindLongestSubstring
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type a string, any string!");
+            var myString = Console.ReadLine();
+            char delimiter = ' ';
+            string[] substrings = myString.Split(delimiter);
+            var longestSubstring = "";
+
+            foreach (var substring in substrings)
+            {
+                if (substring.Length > longestSubstring.Length)
+                {
+                    longestSubstring = substring;
+                }
+                
+            }
+
+            Console.WriteLine("The longest substring is \"{0}\", which is {1} characters long.", longestSubstring, longestSubstring.Length);
+        }
+    }
+}
